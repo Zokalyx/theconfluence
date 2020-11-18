21b6275f83460a42af17df54c4dc03aac53e56f3
Zokalyx <zokalyx@gmail.com>
2020-11-18 13:59:04 -0300

    First heroku test

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: bot/checkcomment.py
---
+++ bot/checkcomment.py
@@ -0,0 +1,89 @@
+import praw
+import time
+import csv
+import math
+from os import environ
+
+s = time.time()  # time
+smm = s - 2592000  # time minus one month
+
+names = []
+bools = []
+links = []
+times = []
+wk = 0
+with open("../csv/pro.csv", "r") as pro:
+    reader = csv.reader(pro)
+    for row in reader:
+        if not wk:
+            wk = len(row)
+        names.append(row[-1])
+        bools.append(False)
+        links.append("")
+        times.append(0)
+
+with open("shh.txt", "r") as secret:
+    secrets = secret.readlines()
+
+reddit = praw.Reddit(client_id=environ["CLIENT_ID"],
+                     client_secret=environ["CLIENT_SECRET"],
+                     username=environ["USERNAME"],
+                     password=environ["PASSWORD"],
+                     user_agent="TheConfluenceBot")
+
+last_run_start = 0
+sub = reddit.subreddit("TheConfluence")
+runs = sub.hot(limit=5)
+posts = sub.new(limit=300)
+
+for run in runs:
+    if run.stickied:
+        if " Run " in run.title:
+            print(run.title)
+            last_run_start = run.created_utc
+            break
+
+for post in posts:
+    if post.created_utc > smm:
+        if post.created_utc > last_run_start:
+            # print(post.title)
+            print(str(math.trunc(10*((s - post.created_utc) / (s - last_run_start) * 100))/10) + "%")
+            if post.author == "yo-whatupmofo":
+                ind = names.index("Or-Your-Money-Back")
+                if not bools[ind]:
+                    links[ind] = "reddit.com" + post.permalink
+                    times[ind] = (s - post.created_utc)/86400
+                bools[ind] = True
+            if post.author in names:
+                ind = names.index(post.author)
+                if not bools[ind]:
+                    links[ind] = "reddit.com" + post.permalink
+                    times[ind] = (s - post.created_utc)/86400
+                bools[ind] = True
+            comments = post.comments
+            comments.replace_more(limit=None)
+            for comment in comments.list():
+                if comment.created_utc > last_run_start:
+                    if comment.author == "yo-whatupmofo":
+                        ind = names.index("Or-Your-Money-Back")
+                        if not bools[ind]:
+                            links[ind] = "reddit.com" + post.permalink
+                            times[ind] = (s - post.created_utc) / 86400
+                        bools[ind] = True
+                    if comment.author in names:
+                        ind = names.index(comment.author)
+                        if not bools[ind]:
+                            links[ind] = "reddit.com" + post.permalink
+                            times[ind] = (s - comment.created_utc)/86400
+                        bools[ind] = True
+    else:
+        break
+
+com = zip(names, bools, links, times)
+
+with open("commented.csv", "w", newline="") as commented:
+    writer = csv.writer(commented)
+    writer.writerows(com)
+
+with open("wk.txt", "w") as wek:
+    wek.write(str(wk - 1))
