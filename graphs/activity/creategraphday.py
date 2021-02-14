cf11ea0e4651fdae1dc67628bef2f6dbe39b1ded
Zokalyx <zokalyx@gmail.com>
2021-02-14 16:11:21 -0300

    added time activity

---
+++ graphs/activity/creategraphday.py
@@ -0,0 +1,168 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle, RegularPolygon
+from matplotlib.path import Path
+from matplotlib.projections.polar import PolarAxes
+from matplotlib.projections import register_projection
+from matplotlib.spines import Spine
+from matplotlib.transforms import Affine2D
+
+import praw
+
+with open("../leaderboard/shh.txt", "r") as secret:
+    secrets = secret.readlines()
+
+reddit = praw.Reddit(client_id="xskzciRXmoU-JA",
+                     client_secret=secrets[1],
+                     username="Zokalyx",
+                     password=secrets[0].strip(),
+                     user_agent="theconfluenceBOT")
+sub = reddit.subreddit("TheConfluence")
+posts = sub.new(limit=None)
+totalposts = 0
+totalcomments = 0
+last_utc = 0
+poststime = []
+commentstime = []
+for i in range(7):
+    poststime.append(0)
+    commentstime.append(0)
+
+for post in posts:
+    tim = round((post.created_utc / 86400) % 24)
+    if tim == 24:
+        tim = 0
+    poststime[tim] += 1
+    totalposts += 1
+    print(totalposts)
+
+    comments = post.comments
+    comments.replace_more(limit=None)
+
+    for comment in comments.list():
+        print(comment.body)
+        tim = round((post.created_utc / 3600) % 24)
+        if tim == 24:
+            tim = 0
+        commentstime[tim] += 1
+        totalcomments += 1
+
+    last_utc = post.created_utc
+
+print("Last post:")
+print(last_utc)
+
+for i in range(24):
+    poststime[i] /= totalposts
+    commentstime[i] /= totalcomments
+
+data = [['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+        ('Time of posts and comments in GMT (+0:00)', [poststime, commentstime])]
+
+
+def radar_factory(num_vars, frame='circle'):
+    """Create a radar chart with `num_vars` axes.
+
+    This function creates a RadarAxes projection and registers it.
+
+    Parameters
+    ----------
+    num_vars : int
+        Number of variables for radar chart.
+    frame : {'circle' | 'polygon'}
+        Shape of frame surrounding axes.
+
+    """
+    # calculate evenly-spaced axis angles
+    theta = np.linspace(0, -2*np.pi, num_vars, endpoint=False)
+
+    class RadarAxes(PolarAxes):
+
+        name = 'radar'
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            # rotate plot such that the first axis is at the top
+            self.set_theta_zero_location('N')
+
+        def fill(self, *args, closed=True, **kwargs):
+            """Override fill so that line is closed by default"""
+            return super().fill(closed=closed, *args, **kwargs)
+
+        def plot(self, *args, **kwargs):
+            """Override plot so that line is closed by default"""
+            lines = super().plot(*args, **kwargs)
+            for line in lines:
+                self._close_line(line)
+
+        def _close_line(self, line):
+            x, y = line.get_data()
+            # FIXME: markers at x[0], y[0] get doubled-up
+            if x[0] != x[-1]:
+                x = np.concatenate((x, [x[0]]))
+                y = np.concatenate((y, [y[0]]))
+                line.set_data(x, y)
+
+        def set_varlabels(self, labels):
+            self.set_thetagrids(np.degrees(theta), labels)
+
+        def _gen_axes_patch(self):
+            # The Axes patch must be centered at (0.5, 0.5) and of radius 0.5
+            # in axes coordinates.
+            if frame == 'circle':
+                return Circle((0.5, 0.5), 0.5)
+            elif frame == 'polygon':
+                return RegularPolygon((0.5, 0.5), num_vars,
+                                      radius=.5, edgecolor="k")
+            else:
+                raise ValueError("unknown value for 'frame': %s" % frame)
+
+        def draw(self, renderer):
+            """ Draw. If frame is polygon, make gridlines polygon-shaped """
+            if frame == 'polygon':
+                gridlines = self.yaxis.get_gridlines()
+                for gl in gridlines:
+                    gl.get_path()._interpolation_steps = num_vars
+            super().draw(renderer)
+
+        def _gen_axes_spines(self):
+            if frame == 'circle':
+                return super()._gen_axes_spines()
+            elif frame == 'polygon':
+                # spine_type must be 'left'/'right'/'top'/'bottom'/'circle'.
+                spine = Spine(axes=self,
+                              spine_type='circle',
+                              path=Path.unit_regular_polygon(num_vars))
+                # unit_regular_polygon gives a polygon of radius 1 centered at
+                # (0, 0) but we want a polygon of radius 0.5 centered at (0.5,
+                # 0.5) in axes coordinates.
+                spine.set_transform(Affine2D().scale(.5).translate(.5, .5)
+                                    + self.transAxes)
+
+
+                return {'polar': spine}
+            else:
+                raise ValueError("unknown value for 'frame': %s" % frame)
+
+    register_projection(RadarAxes)
+    return theta
+
+
+N = len(data[0])
+theta = radar_factory(N, frame='polygon')
+
+spoke_labels = data.pop(0)
+title, case_data = data[0]
+
+fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(projection='radar'))
+fig.subplots_adjust(top=0.85, bottom=0.05)
+
+ax.set_title(title,  position=(0.5, 1.1), ha='center')
+
+for d in case_data:
+    line = ax.plot(theta, d)
+    ax.fill(theta, d,  alpha=0.25)
+ax.set_varlabels(spoke_labels)
+
+plt.savefig("day.png")
