46ebbbbaeab602927460800c4e7361694424d0a6
Zokalyx <zokalyx@gmail.com>
2021-06-16 12:04:16 -0300

    week 67

--- executables/updatevideo.bat
+++ executables/updatevideo.bat
@@ -1,12 +1,12 @@
 cd ..
 cd website
 cd leaderboard
-copy /y names.txt "C:\Users\White Python\Desktop\Fran\ManimCE\leaderboard"
-cd "C:\Users\White Python\Desktop\Fran\ManimCE\venv\Scripts"
+copy /y names.txt "C:\Users\White Python\Desktop\Fran\Coding\ManimCE\leaderboard"
+cd "C:\Users\White Python\Desktop\Fran\Coding\ManimCE\venv\Scripts"
 call activate.bat
-cd "C:\Users\White Python\Desktop\Fran\ManimCE\leaderboard"
+cd "C:\Users\White Python\Desktop\Fran\Coding\ManimCE\leaderboard"
 call manim makevideo.py Render -qh -r 400,1200 --media_dir "C:\Users\White Python\Desktop\Fran\theconfluence\website\videos"
-cd "C:\Users\White Python\Desktop\Fran\ManimCE\venv\Scripts"
+cd "C:\Users\White Python\Desktop\Fran\Coding\ManimCE\venv\Scripts"
 call deactivate.bat
-cd "C:\Users\White Python\Desktop\Fran\theconfluence\website\leaderboard"
+cd "C:\Users\White Python\Desktop\Fran\Coding\theconfluence\website\leaderboard"
 pause
