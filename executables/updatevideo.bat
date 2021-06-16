3c819dae08481fbf509aceaa60f469334adfbf40
Zokalyx <zokalyx@gmail.com>
2021-06-16 13:04:00 -0300

    updated video (rip fifth)

--- executables/updatevideo.bat
+++ executables/updatevideo.bat
@@ -5,7 +5,7 @@ copy /y names.txt "C:\Users\White Python\Desktop\Fran\Coding\ManimCE\leaderboard
 cd "C:\Users\White Python\Desktop\Fran\Coding\ManimCE\venv\Scripts"
 call activate.bat
 cd "C:\Users\White Python\Desktop\Fran\Coding\ManimCE\leaderboard"
-call manim makevideo.py Render -qh -r 400,1200 --media_dir "C:\Users\White Python\Desktop\Fran\theconfluence\website\videos"
+call manim makevideo.py Render -qm -r 2400,800 --media_dir "C:\Users\White Python\Desktop\Fran\theconfluence\website\videos"
 cd "C:\Users\White Python\Desktop\Fran\Coding\ManimCE\venv\Scripts"
 call deactivate.bat
 cd "C:\Users\White Python\Desktop\Fran\Coding\theconfluence\website\leaderboard"
